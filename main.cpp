cbffdde97dbd35cdbb603c403da65efc95c2b9b0
Dominik__ <arkenvoss.dk@gmail.com>
2018-03-14 21:18:14 +0100

    SFML linked and included

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+#pragma warning(disable: 4251)
+
 #include <glbinding/gl45core/gl.h>
 #include <glbinding/Binding.h>
 
-#pragma warning(suppress: 4251)
+#include <SFML/Window.hpp>
+
 
 int main()
 {
 	glbinding::Binding::initialize(false);
+
+	sf::Window window(sf::VideoMode(600, 800), "Illegal Tram Racing");
+
+
 	return 0;
 }
