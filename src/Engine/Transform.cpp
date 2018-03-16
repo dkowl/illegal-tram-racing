afb045db5af7f474b10a9be8c0c783cc40b39d9d
Dominik__ <arkenvoss.dk@gmail.com>
2018-03-16 17:11:41 +0100

    Transform hierarchy

--- src/Engine/Transform.cpp
+++ src/Engine/Transform.cpp
@@ -1,51 +1,95 @@
 #include "Transform.h"
 
 Transform::Transform():
-	position(0, 0, 0),
-	scale(1, 1, 1)
+	localPosition(0, 0, 0),
+	localScale(1, 1, 1)
 {
 }
 
 glm::mat4 Transform::ModelMatrix() const
 {
 	glm::mat4 result;
-	result = glm::translate(result, position);
-	result *= glm::mat4_cast(rotation);
-	result = glm::scale(result, scale);
+	result = glm::translate(result, worldPosition);
+	result *= glm::mat4_cast(worldRotation);
+	result = glm::scale(result, worldScale);
 	return result;
 }
 
-void Transform::SetPosition(const glm::vec3 &position)
+glm::vec3 Transform::Position() const
 {
-	this->position = position;
+	return worldPosition;
+}
+
+glm::quat Transform::Rotation() const
+{
+	return worldRotation;
+}
+
+glm::vec3 Transform::Scale() const
+{
+	return worldScale;
+}
+
+void Transform::SetLocalPosition(const glm::vec3 &position)
+{
+	this->localPosition = position;
+	Update();
 }
 
 void Transform::Move(const glm::vec3 &offset)
 {
-	this->position += offset;
+	this->localPosition += offset;
+	Update();
 }
 
-void Transform::SetRotation(const float &degrees, const glm::vec3 &axis)
+void Transform::SetLocalRotation(const float &degrees, const glm::vec3 &axis)
 {
 	glm::mat4 rotationMatrix;
 	rotationMatrix = glm::rotate(rotationMatrix, glm::radians(degrees), axis);
-	rotation = glm::quat_cast(rotationMatrix);
+	localRotation = glm::quat_cast(rotationMatrix);
+	Update();
 }
 
 void Transform::Rotate(const float& degrees, const glm::vec3& axis)
 {
-	glm::mat4 rotationMatrix = glm::rotate(glm::mat4_cast(rotation), glm::radians(degrees), axis);
-	rotation = glm::quat_cast(rotationMatrix);
+	glm::mat4 rotationMatrix = glm::rotate(glm::mat4_cast(localRotation), glm::radians(degrees), axis);
+	localRotation = glm::quat_cast(rotationMatrix);
+	Update();
 }
 
-void Transform::SetScale(const glm::vec3& scale)
+void Transform::SetLocalScale(const glm::vec3& scale)
 {
-	this->scale = scale;
+	this->localScale = scale;
+	Update();
 }
 
 void Transform::Scale(const glm::vec3& scale)
 {
-	this->scale = glm::vec3(this->scale.x * scale.x, this->scale.y * scale.y, this->scale.z * scale.z);
+	this->localScale = glm::vec3(this->localScale.x * scale.x, this->localScale.y * scale.y, this->localScale.z * scale.z);
+	Update();
+}
+
+void Transform::Update()
+{
+	auto parentShared = parent.lock();
+	if(parentShared)
+	{
+		worldPosition = parentShared->worldPosition + localPosition;
+		worldRotation = parentShared->worldRotation * localRotation;
+		worldScale = parentShared->worldScale * localScale;		
+	}
+	else
+	{
+		worldPosition = localPosition;
+		worldRotation = localRotation;
+		worldScale = localScale;
+	}
+
+	for(auto&& child: children)
+	{
+		auto childShared = child.lock();
+		if (childShared) childShared->Update();
+	}
 }
 
 
--- src/Engine/Transform.h
+++ src/Engine/Transform.h
@@ -1,26 +1,45 @@
 #pragma once
+#include <vector>
+#include <memory>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 class Transform
 {
-	glm::vec3 position;
-	glm::quat rotation;
-	glm::vec3 scale;
+	glm::vec3 localPosition;
+	glm::quat localRotation;
+	glm::vec3 localScale;
+
+	glm::vec3 worldPosition;
+	glm::quat worldRotation;
+	glm::vec3 worldScale;
+
+	std::weak_ptr<Transform> parent;
+	std::vector<std::weak_ptr<Transform>> children;
 
 public:
 	Transform();
 
 	glm::mat4 ModelMatrix() const;
 
-	void SetPosition(const glm::vec3 &position);
+	glm::vec3 Position() const;
+	glm::quat Rotation() const;
+	glm::vec3 Scale() const;
+
+	void SetLocalPosition(const glm::vec3 &position);
 	void Move(const glm::vec3 &offset);
 
-	void SetRotation(const float &degrees, const glm::vec3 &axis);
+	void SetLocalRotation(const float &degrees, const glm::vec3 &axis);
 	void Rotate(const float &degrees, const glm::vec3 &axis);
 
-	void SetScale(const glm::vec3 &scale);
+	void SetLocalScale(const glm::vec3 &scale);
 	void Scale(const glm::vec3 &scale);
+
+
+
+private:
+	void Update();
 
 };
